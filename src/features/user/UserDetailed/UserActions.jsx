8cbf266fedf012f189147afa7450398783d3f15c
Johan Quiroz <johan_9_31@hotmail.com>
2019-04-25 13:05:21 -0700

    Photo upload to Firebase

--- src/features/user/UserDetailed/UserActions.jsx
+++ src/features/user/UserDetailed/UserActions.jsx
@@ -1,5 +1,5 @@
-
 import moment from 'moment';
+import cuid from 'cuid';
 import { toastr } from 'react-redux-toastr'
 
 export const updateProfile = (user) =>
@@ -20,7 +20,41 @@ async (dispatch, getState, {getFirebase}) => {
 
 export const uploadProfileImage = (file, fileName) =>
     async (dispatch, getState, {getFirebase, getFirestore}) => {
+        const imageName = cuid();
         const firebase = getFirebase();
         const firestore = getFirestore();
         const user = firebase.auth().currentUser;
+        const path = `${user.uid}/user_images`;
+        const options = {
+            name: imageName
+        };
+        try{
+            //upload the file to firebase storage
+            let uploadedFile = await firebase.uploadFile(path, file, null, options);
+            //get url of image
+            let downloadURL = await uploadedFile.uploadTaskSnapshot.downloadURL;
+            //get userdoc
+            let userDoc = await firestore.get(`users/${user.uid}`);
+            //check if user has photo, if not update profile with new image
+            if (!userDoc.data().photoURL) {
+                await firebase.updateProfile({
+                    photoURL: downloadURL
+                });
+                await user.updateProfile({
+                    photoURL: downloadURL
+                })
+            };
+            //add the new photo to photos collection
+            return await firestore.add({
+                collection: 'users',
+                doc: user.uid,
+                subcollections: [{collection: 'photos'}]
+            }, {
+                name: imageName,
+                url: downloadURL
+            })
+        }catch(error){
+            console.log(error);
+            throw new Error('Problem uploading photo');
+        }
     }
