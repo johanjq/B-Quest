da820d2a5d74af74c1f6dc55959b8488021b005a
Johan Quiroz <johan_9_31@hotmail.com>
2019-04-29 07:01:12 -0700

    Changed some styling

--- src/features/event/THActivity/THActivity.jsx
+++ src/features/event/THActivity/THActivity.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Header, Segment } from 'semantic-ui-react'
 
+
 const THActivity = () => {
     return(
         <div>
             <Header attached='top' content='Recent Activity' />
             <Segment attached>
-                <p>Recent Activity</p>
+               
             </Segment>
         </div>
     )
